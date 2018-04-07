0e0e6615acff153865c3b60e542bc48a37e2d08d
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 04:30:45 +1000

    Control Panel and CMD.exe items

--- recipes/user-experience.rb
+++ recipes/user-experience.rb
@@ -4,9 +4,30 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-# IE Enhanced Security Configuration: Off (or install Chrome)
-# Taskbar properties > Taskbar Buttons > Never Combined
-# Notification Area Icons > Always show all icons
-# Control panel set to use Small Icons layout
-# cmd.exe layout - width to 160, height to 50
-# Set network profile to private
+#- IE Enhanced Security Configuration: Off (or install Chrome)
+#- Taskbar properties > Taskbar Buttons > Never Combined
+#- Notification Area Icons > Always show all icons
+
+#- Set network profile to private
+
+
+#- Control panel set to use Small Icons layout
+# 1 = small icons, 0 = large icons
+registry_key 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel' do
+   values [{
+     name: 'AllItemsIconView',
+     type: :dword,
+     data: 1,
+   }]
+   action :create
+end
+
+# cmd.exe layout - width to 150, height to 45
+registry_key 'HKEY_CURRENT_USER\Console' do
+   values [{
+     name: 'WindowSize',
+     type: :dword,
+     data: 2d0096,
+   }]
+   action :create
+end
