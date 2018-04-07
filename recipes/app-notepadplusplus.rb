302d51e232e10106a20797d9253d48c8fc197ff0
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 04:31:21 +1000

    added first pass

--- recipes/app-notepadplusplus.rb
+++ recipes/app-notepadplusplus.rb
@@ -3,3 +3,13 @@
 # Recipe:: app-notepadplusplus
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# Download and Unzip the file
+seven_zip_archive 'NotepadPlusPlus.zip' do
+  source 'https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.bin.x64.zip'
+  path 'C:\Program Files (x86)\Notepad++'
+  overwrite true
+end
+
+
+""
