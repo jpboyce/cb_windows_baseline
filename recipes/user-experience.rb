2a62c030c0110deb9d4d49a30509d53bdeec19cf
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 03:33:23 +1000

    placeholder headings

--- recipes/user-experience.rb
+++ recipes/user-experience.rb
@@ -3,3 +3,10 @@
 # Recipe:: user-experience
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# IE Enhanced Security Configuration: Off (or install Chrome)
+# Taskbar properties > Taskbar Buttons > Never Combined
+# Notification Area Icons > Always show all icons
+# Control panel set to use Small Icons layout
+# cmd.exe layout - width to 160, height to 50
+# Set network profile to private
