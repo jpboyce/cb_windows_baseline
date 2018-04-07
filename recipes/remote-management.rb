90b0cb8074072fa49f3a30bce0f388b40a1100a4
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 03:27:16 +1000

    chef generate recipe

---
+++ recipes/remote-management.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_windows_baseline
+# Recipe:: remote-management
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ recipes/user-experience.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_windows_baseline
+# Recipe:: user-experience
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
