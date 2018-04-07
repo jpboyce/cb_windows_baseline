dd2daff5875f2b9add4dfe4d37bca3487816bde1
Jesse Boyce <jesse@jpboyce.org>
2018-04-08 03:32:57 +1000

    chef generate recipe

---
+++ recipes/app-notepadplusplus.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_windows_baseline
+# Recipe:: app-notepadplusplus
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ recipes/app-sysinternals.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_windows_baseline
+# Recipe:: app-sysinternals
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
