1552b3784c8a8f07dc30632b5835fb30c0728c7e
Jesse Boyce <jesse@jpboyce.org>
2019-01-08 05:25:37 +1000

    Testing chef handlers

---
+++ recipes/chef-handlers.rb
@@ -0,0 +1,17 @@
+# Chef Handlers Recipe
+# Adding certain handlers in
+
+# Include chef_handler
+include_recipe "chef_handler"
+
+# Install the gem
+chef_gem 'chef-handler-elapsed-time'
+
+# Load the gem
+require 'chef/handler/elapsed_time'
+
+# Activate the handler
+chef_handler 'Chef::Handler::ElaspedTime' do
+  source 'chef/handler/elapsed_time'
+  action :nothing
+end.run_action(:enable)
