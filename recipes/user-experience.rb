ab999ffb464de5f0a77ba3161a9313ef69b0e18c
Jesse <jesse@jpboyce.org>
2019-05-21 19:40:31 +1000

    alignment fixes

--- recipes/user-experience.rb
+++ recipes/user-experience.rb
@@ -8,21 +8,21 @@
 
 #- Notification Area Icons > Always show all icons
 registry_key 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer' do
-   values [{
-     name: 'NoAutoTrayNotify',
-     type: :dword,
-     data: 1,
+  values [{
+    name: 'NoAutoTrayNotify',
+    type: :dword,
+    data: 1,
   }]
-   action :create
+  action :create
 end
 
 #- Taskbar properties > Taskbar Buttons > Never Combined
 # Credit: http://blog.unlockforus.org/2012/05/windows-7-start-menu-taskbar-appearance.html
 registry_key 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' do
-   values [{
-     name: 'TaskbarGlomLevel',
-     type: :dword,
-     data: 2,
+  values [{
+    name: 'TaskbarGlomLevel',
+    type: :dword,
+    data: 2,
   }]
    action :create
 end
@@ -30,20 +30,20 @@ end
 #- Control panel set to use Small Icons layout
 # 1 = small icons, 0 = large icons
 registry_key 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel' do
-   values [{
-     name: 'AllItemsIconView',
-     type: :dword,
-     data: 1,
+  values [{
+    name: 'AllItemsIconView',
+    type: :dword,
+    data: 1,
   }]
    action :create
 end
 
 # cmd.exe layout - width to 150, height to 45
 registry_key 'HKEY_CURRENT_USER\Console' do
-   values [{
-     name: 'WindowSize',
-     type: :dword,
-     data: '2d0096',
+  values [{
+    name: 'WindowSize',
+    type: :dword,
+    data: '2d0096',
   }]
    action :create
 end
@@ -51,10 +51,10 @@ end
 # IE Enhanced Security Configuration for Admins: Off
 # Credit: https://4sysops.com/archives/four-ways-to-disable-internet-explorer-enhanced-security-configuration-ie-esc/
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' do
-   values [{
-     name: 'IsInstalled',
-     type: :dword,
-     data: 0,
+  values [{
+    name: 'IsInstalled',
+    type: :dword,
+    data: 0,
   }]
    action :create
 end
@@ -62,10 +62,10 @@ end
 # IE Enhanced Security Configuration for Users: Off
 # Credit: https://4sysops.com/archives/four-ways-to-disable-internet-explorer-enhanced-security-configuration-ie-esc/
 registry_key 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' do
-   values [{
-     name: 'IsInstalled',
-     type: :dword,
-     data: 0,
+  values [{
+    name: 'IsInstalled',
+    type: :dword,
+    data: 0,
   }]
    action :create
 end
