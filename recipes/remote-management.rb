b74b64b1c10938307ff982baa587cb73c5a95774
Jesse <jesse@jpboyce.org>
2019-05-21 19:47:55 +1000

    .

--- recipes/remote-management.rb
+++ recipes/remote-management.rb
@@ -6,12 +6,12 @@
 
 # Enable RDP
 registry_key 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server' do
-   values [{
-     name: 'fDenyTSConnection',
-     type: :dword,
-     data: 0,
-   }]
-   action :create
+  values [{
+    name: 'fDenyTSConnection',
+    type: :dword,
+    data: 0,
+  }]
+  action :create
 end
 
 # Enable Remote Management
