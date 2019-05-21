088639912b064018abb88d7be2611ac3ad5d025c
Jesse <jesse@jpboyce.org>
2019-05-21 20:16:11 +1000

    .

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Installs/Configures cb_windows_baseline'
 long_description 'Installs/Configures cb_windows_baseline'
 version '0.1.0'
-chef_version '>= 12.1', '< 15.0' if respond_to?(:chef_version)
+chef_version '>= 12.1', '< 15.0.0' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
